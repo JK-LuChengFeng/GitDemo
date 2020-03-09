33dfa9ef7ebeba183d43ca19e4b3d8dbda8a14e3
JK <1030088968@qq.com>
2020-03-09 11:01:01 +0800

    TestBranchVersion1.0

---
+++ app/src/main/java/com/example/gittestdemo/Main3Activity.java
@@ -0,0 +1,17 @@
+package com.example.gittestdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * Create By JK_Liu on 2020/03/09
+ */
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
