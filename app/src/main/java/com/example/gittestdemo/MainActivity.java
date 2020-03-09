d98249b94412b7aa697116fc59071076a6e205d8
jk_liu <1115170755@qq.com>
2020-03-09 14:06:17 +0800

    Another Count Commit Test1

--- app/src/main/java/com/example/gittestdemo/MainActivity.java
+++ app/src/main/java/com/example/gittestdemo/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.gittestdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Create By JK_Liu on ${DATE}
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btn_jump;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +29,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-
+        btn_jump = findViewById(R.id.btn_jump);
+        btn_jump.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_jump:
+                startActivity(new Intent(MainActivity.this,Main2Activity.class));
+                break;
+        }
+    }
 }
