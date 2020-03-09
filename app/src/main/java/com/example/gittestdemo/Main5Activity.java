fb859af5a8a89e703dc01d0fa59fc3305f0eca31
JK-Lu9121 <1115170755@qq.com>
2020-03-09 15:27:44 +0800

    主分支提交

--- app/src/main/java/com/example/gittestdemo/Main5Activity.java
+++ app/src/main/java/com/example/gittestdemo/Main5Activity.java
@@ -3,12 +3,14 @@ package com.example.gittestdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * Create By JK_Liu on ${DATE}
  */
 public class Main5Activity extends AppCompatActivity {
 
+    private TextView tv_ceshi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
