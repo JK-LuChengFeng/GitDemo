bc58d5f9eecb40b4176c355d09cf95f1f98f80d8
JK-LuChengFeng <97685406@qq.com>
2020-03-09 15:21:07 +0800

    测试主分支

---
+++ app/src/main/java/com/example/gittestdemo/Main6Activity.java
@@ -0,0 +1,17 @@
+package com.example.gittestdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * Create By JK_Liu on ${DATE}
+ */
+public class Main6Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main6);
+    }
+}
