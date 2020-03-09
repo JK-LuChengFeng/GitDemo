c16aa4f04a8469daae7f6c48450880a8a346852c
JK <1030088968@qq.com>
2020-03-09 10:56:33 +0800

    First Commit

---
+++ app/src/main/java/com/example/gittestdemo/Main2Activity.java
@@ -0,0 +1,32 @@
+package com.example.gittestdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Create By JK_Liu on 2020/03/09
+ */
+public class Main2Activity extends AppCompatActivity {
+
+    private TextView tv_branchtest;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        initData();
+        initView();
+
+    }
+
+    private void initData() {
+        
+    }
+
+    private void initView() {
+
+    }
+}
---
+++ app/src/main/java/com/example/gittestdemo/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.gittestdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * Create By JK_Liu on ${DATE}
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initData();
+        initView();
+
+    }
+
+    private void initData() {
+
+    }
+
+    private void initView() {
+
+    }
+
+}
